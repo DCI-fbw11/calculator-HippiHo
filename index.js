0794c90275f529cc02ac3530e5f1c08396db2f91
Elise Maschke <elise.maschke@outlook.de>
2019-03-27 11:30:44 +0100

    solution

--- index.js
+++ index.js
@@ -1,22 +1,74 @@
 class Calc {
   constructor(num) {
-    this.num = num
+    this.num = num;
   }
   add(numToAdd) {
-    this.throwOnBadDatatype(numToAdd)
-    this.num = this.num + numToAdd
-    return this
+    this.throwOnBadDatatype(numToAdd);
+    this.num = this.num + numToAdd;
+    return this;
   }
 
-  throwOnBadDatatype(numToAdd) {
-    if (typeof numToAdd === "string") {
-      throw new Error("No strings allowed")
+  plus(num) {
+    return this.add(num);
+  }
+
+  subtract(numToSubtract) {
+    this.throwOnBadDatatype(numToSubtract);
+    this.num = this.num - numToSubtract;
+    return this;
+  }
+
+  minus(num) {
+    this.subtract(num);
+  }
+
+  multiply(numToMultiply) {
+    this.throwOnBadDatatype(numToMultiply);
+    this.num = this.num * numToMultiply;
+    return this;
+  }
+
+  asterisk(num) {
+    this.multiply(num);
+  }
+
+  divide(numToDivide) {
+    this.throwOnBadDatatype(numToDivide);
+    this.num = this.num / numToDivide;
+    return this;
+  }
+
+  slash(num) {
+    this.divide(num);
+  }
+
+  throwOnBadDatatype(nextNum) {
+    if (typeof nextNum === "string") {
+      throw new Error("No strings allowed");
     }
 
-    if (typeof numToAdd === "object") {
-      throw new Error("No object allowed")
+    //This check needs to come before the object test, because typeOf returns object for arrays
+
+    if (Array.isArray(nextNum) === true) {
+      throw new Error("No array allowed");
     }
+
+    if (typeof nextNum === "object") {
+      throw new Error("No object allowed");
+    }
+
+    if (typeof nextNum === "boolean") {
+      throw new Error("No boolean allowed");
+    }
+
+    if (typeof nextNum === "undefined") {
+      throw new Error("No undefined allowed");
+    }
+  }
+
+  result() {
+    return this.num;
   }
 }
 
-module.exports = Calc
+module.exports = Calc;
--- index.test.js
+++ index.test.js
@@ -1,21 +1,68 @@
-const Calc = require("./index")
+const Calc = require("./index");
 
 describe("Calculator tests", () => {
   test("should be able to add 1 + 1", () => {
-    expect(new Calc(1).add(1)).toEqual({ num: 2 })
-  })
+    expect(new Calc(1).add(1)).toEqual({ num: 2 });
+  });
 
   test("should be able to add multiple numbers", () => {
-    expect(new Calc(1).add(1).add(1)).toEqual({ num: 3 })
-  })
+    expect(new Calc(1).add(1).add(1)).toEqual({ num: 3 });
+  });
 
-  test("Dont accept strings", () => {
-    expect(() => new Calc(100).add("onehundret")).toThrow("No strings allowed")
-  })
+  test("should be able to subtract 3 - 1", () => {
+    expect(new Calc(3).subtract(1)).toEqual({ num: 2 });
+  });
 
-  test("Dont accept object", () => {
+  test("should be able to subtract multiple numbers", () => {
+    expect(new Calc(3).subtract(1).subtract(1)).toEqual({ num: 1 });
+  });
+
+  test("should be able to multiply 3 * 1", () => {
+    expect(new Calc(3).multiply(1)).toEqual({ num: 3 });
+  });
+
+  test("should be able to multiply multiple numbers", () => {
+    expect(new Calc(3).multiply(1).multiply(2)).toEqual({ num: 6 });
+  });
+
+  test("should be able to divide 4 / 2", () => {
+    expect(new Calc(4).divide(2)).toEqual({ num: 2 });
+  });
+
+  test("should be able to divide multiple numbers", () => {
+    expect(new Calc(4).divide(2).divide(2)).toEqual({ num: 1 });
+  });
+
+  test("Don't accept strings", () => {
+    expect(() => new Calc(100).add("onehundret")).toThrow("No strings allowed");
+  });
+
+  test("Don't accept object", () => {
     expect(() => new Calc(100).add({ num: "onehundret" })).toThrow(
       "No object allowed"
-    )
-  })
-})
+    );
+  });
+
+  test("Don't accept boolean", () => {
+    expect(() => new Calc(100).add(false)).toThrow("No boolean allowed");
+  });
+
+  test("Don't accept undefined", () => {
+    expect(() => new Calc(100).add(undefined)).toThrow("No undefined allowed");
+  });
+
+  test("Don't accept array", () => {
+    expect(() => new Calc(100).add([100, 100])).toThrow("No array allowed");
+  });
+
+  test("Whole calculation", () => {
+    expect(
+      new Calc(1)
+        .add(3)
+        .subtract(3)
+        .multiply(2)
+        .divide(3)
+        .result()
+    ).toEqual(2 / 3);
+  });
+});
